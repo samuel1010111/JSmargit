f54082f7c66fdccdf1660152eedebd7248573922
samuel1010111 <samuel.t.abebe@gmail.com>
2023-12-09 23:50:11 +0200

    All necessary changes made for all the tasks

--- POK/main.js
+++ POK/main.js
@@ -1,49 +1,101 @@
-const content = document.querySelector('.pokedex');
+// Defining variables to select HTML elements.
+const searchInput = document.querySelector('#search-input');
+const generationList = document.querySelector('#generation-list');
+const pokemonCards = document.querySelector('#pokemon-cards');
+// Fetching a list of generations from the PokeAPI.
+fetch("https://pokeapi.co/api/v2/generation/")
+.then(response => response.json())
+.then(data => {
+    const generations = data.results;
 
-let pokeData = [1, 2, 3];
-
-const fetchData = async () => {
-  await fetch('https://pokeapi.co/api/v2/pokemon?limit=121&offset=0')
-    .then((response) => response.json())
-    .then((data) => {
-      const fetches = data.results.map((item) => {
-        return fetch(item.url)
-          .then((res) => res.json())
-          .then((data) => {
-            return {
-              id: data.id,
-              name: data.name,
-              img: data.sprites.other['official-artwork'].front_default,
-              types: data.types,
-            };
-          });
-      });
-      Promise.all(fetches).then((res) => {
-        pokeData = res;
-        pokeCards();
-      });
+    // Creating buttons for each generation
+    generations.forEach(generation => {
+        const button = document.querySelector('.button');
+        button.textContent = generation.name;
+        button.addEventListener("click", () => loadPokemonByGeneration(generation.name));
+        generationList.appendChild(button);
     });
-};
+});
 
-// TODO: 
-// 1. pokemon.types is an array, you need to map it again
-// 2. connect input and search from pokeDex array by using the .filter() method (https://www.jamesqquick.com/blog/build-a-javascript-search-bar/) 
+// Function to load Pokemon by generation.
+function loadPokemonByGeneration(generationName) {
+    // Fetching Pokemon data for the selected generation.
+fetch(`https://pokeapi.co/api/v2/generation/${generationName}/`)
+.then(response => response.json())
+.then(data => {
+const pokemonList = data.pokemon_species;
 
-const pokeCards = () => {
-  const cards = pokeData
-    .map((pokemon) => {
-      return `<div>
-  <img src="${pokemon.img}" alt="${pokemon.name}"/>
-  <p>${pokemon.id}</p>
-  <h2>${pokemon.name}</h2>
-  <div>
-    <p>Water</p>
-    <p>Poison</p>
-  </div>
-</div>`;
-    }).join('');
+// Clearing the existing Pokemon cards.
+pokemonCards.innerHTML = "";
 
-  content.innerHTML = cards;
-};
+// Creating cards for each Pokemon.
+pokemonList.forEach(pokemon => {
+const pokemonName = pokemon.name;
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
+.then(response => response.json())
+.then(pokemonData => {
+// Creating and populating a card for the Pokemon.
+const card = createPokemonCard(pokemonData);
+pokemonCards.appendChild(card);
+});
+});
+});
 
-fetchData();
+// Function creating a Pokemon card.
+function createPokemonCard(pokemon) {
+    // Creating and styling the card element.
+    const card = document.createElement("div");
+    card.classList.add("pokemon-card");
+
+    // Populate the card with data (id, name, image, type, height, weight)
+    const id = pokemon.id;
+    const name = pokemon.name;
+    const image = pokemon.sprites.front_default;
+    const types = pokemon.types.map(type => type.type.name).join(", ");
+    const height = pokemon.height;
+    const weight = pokemon.weight;
+
+    // You can use pokemon.id, pokemon.name, pokemon.sprites.front_default, pokemon.types, pokemon.height, pokemon.weight
+    card.innerHTML = `
+    <h2>${name} (#${id})</h2>
+    <img src="${image}" alt="${name}" />
+    <p>Type: ${types}</p>
+    <p>Height: ${height / 10} m</p>
+    <p>Weight: ${weight / 10} kg
+`;
+    return card;
+}
+
+// Adding event listener for the search input
+searchInput.addEventListener("input", () => searchPokemonByName(searchInput.value));
+
+// Function to search for a specific Pokemon by name.
+function searchPokemonByName(name) {
+    if (!generationList.querySelector('.selected')) {
+        // If no generation is selected, you can fetch the data from the main API
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            .then(response => {
+                if (response.status === 404) {
+                    // Handle the case where the Pokemon is not found
+                    pokemonCards.innerHTML = "<p>Pokemon not found.</p>";
+                } else {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                // Clearing the existing Pokemon cards.
+                pokemonCards.innerHTML = "";
+                // Creating and populating a card for the searched Pokemon.
+                const card = createPokemonCard(data);
+                pokemonCards.appendChild(card);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+    } else {
+        // If a generation is selected, call the loadPokemonByGeneration function
+        const selectedGeneration = generationList.querySelector('.selected').textContent;
+        loadPokemonByGeneration(selectedGeneration);
+    }
+}
+}
--- SpeedGame/main.js
+++ SpeedGame/main.js
@@ -1,22 +1,102 @@
-const form = document.querySelector('form')
-const color1 = document.querySelector('#color1')
-const color2 = document.querySelector('#color2')
-const direction = document.querySelectorAll('input[name="direction"]')
-const answer = document.querySelector('.code')
+const startButton = document.querySelector('#startButton')
+const endButton = document.querySelector('#endButton')
+const circles = document.querySelectorAll('.circle')
+const scoreDisplay = document.querySelector('.score')
+const overlay = document.querySelector("#overlay");
+const resultText = document.querySelector("#result");
+const closeButton = document.querySelector("#close");
+const scoreText = document.querySelector("#score");
 
-const bgGenerator = () =>{
-    let selectedValue;
 
-    for(const item of direction){
-        if(item.checked){
-            selectedValue = item.value
-        }
+
+// global variables
+let score = 0;
+let timer;
+let pace = 1000;
+let active = 0;
+let rounds = 0;
+
+// code from W3S page for the random number 
+/* function getRndInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} */
+
+const getRndInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const clickSound = new Audio('j.mp3');
+const clickCircle = (i) => {
+    clickSound.play();
+  if (i !== active) {
+    return endGame()
+  }
+  rounds--
+  score += 10
+
+  scoreDisplay.textContent = score
+  if(score <20){
+    console.log(good)
+  }
+
+  console.log('button is clicked')
+  clickSound.play()
+}
+
+circles.forEach((circle, i) => {
+  circle.addEventListener('click', () => clickCircle(i))
+})
+
+// same functionality as forEach() but for...of instead
+/* for (const [i, item] of circles.entries()) {
+  item.addEventListener('click', () => clickCircle(i))
+} */
+
+const enableEvents = () => {
+  circles.forEach(circle => {
+    circle.style.pointerEvents = "auto"
+  })
+}
+
+const startGame = () => {
+  if (rounds >= 1) {
+    return endGame()
+  }
+
+  enableEvents();
+  const newActive = pickNew(active)
+
+  circles[newActive].classList.toggle('active')
+  circles[active].classList.remove('active')
+
+  active = newActive;
+
+  timer = setTimeout(startGame, pace)
+
+  pace -= 10
+  rounds++
+  function pickNew(active) {
+    const newActive = getRndInt(0, 3)
+    if (newActive !== active) {
+      return newActive
     }
-    let gradient = `linear-gradient(${selectedValue}, ${color1.value}, ${color2.value})`
-    document.body.style.backgroundImage = gradient
+    return pickNew(active)
+  }
+}
 
-    console.log(gradient);
+const endGame = () => {
+  clearTimeout(timer);
+  overlay.style.visibility = "visible";
+
+  if (score == 0) {
+    resultText.textContent = `Your score is ${score}. Oh come on, you can do better than this!`;
+  } else if (score > 1 || score < 10) {
+    resultText.textContent = `Your score is ${score}, not bad, not bad at all`;
+  } else if (score > 11) {
+    resultText.textContent = `Your score is ${score}, now we are talking!`;
+  }
+};
+const resetGame = () => {
+  window.location.reload()
 }
 
 
-form.addEventListener('change',bgGenerator)
+startButton.addEventListener('click', startGame)
+endButton.addEventListener('click', endGame)
